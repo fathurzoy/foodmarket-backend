edd8dc9a0c072b71dbdf9386386f53abc25db6a8
fathurzoy <fathurrahmanf123@gmail.com>
2021-09-29 17:41:46 +0700

    API Register

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Actions\Fortify\PasswordValidationRules;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use App\Models\User;
@@ -12,6 +13,9 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    //library bawaan laravel
+    use PasswordValidationRules;
+
     //fungsi login
     public function login(Request $request)
     {
@@ -43,8 +47,44 @@ class UserController extends Controller
                 'access_token' => $tokenResult,
                 'token_type' => 'Bearer',
                 'user' => $user
-            ], 'Authenticated')
+            ], 'Authenticated');
 
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something when wrong',
+                'error' => $error
+            ], 'Authentication Failed', 500);
+        }
+    }
+    
+    public function register(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => $this->passwordRules()
+            ]);
+
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'address' => $request->address,
+                'houseNumber' => $request->houseNumber,
+                'phoneNumber' => $request->phoneNumber,
+                'city' => $request->city,
+                'password' => Hash::make($request->pasword),
+            ]);
+
+            $user = User::where('email', $request->email)->first();
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ]);
 
         } catch (Exception $error) {
             return ResponseFormatter::error([
